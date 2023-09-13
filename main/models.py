41b6afc8b326aa0d3046ff8b9ede192cf684bffb
Belati <belatijagadbintangsyuhada@gmail.com>
2023-09-13 11:43:54 +0700

    Unit testing

--- main/models.py
+++ main/models.py
@@ -4,4 +4,5 @@ from django.db import models
 class Item(models.Model):
     name = models.CharField(max_length=32)
     amount = models.IntegerField()
+    rarity = models.IntegerField()
     description = models.TextField()
